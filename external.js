3f97ad01c2dc6623c18ec003268ab66e07e986ee
Mercylina01 <kuteesa.mercylina10@gmail.com>
2026-03-26 17:12:06 +0300

    javascript datatypes

---
+++ external.js
@@ -0,0 +1,53 @@
+console.log("My name is Mercylina");
+
+//Declaring variables
+//Declaring variables we var,let,const
+var name = "Mercylina";
+console.log(name);
+name = "Mercy";
+console.log(name);
+//number data type
+let age = 25;
+console.log(age);
+age = 26;
+console.log("My age has been changed ",age);
+
+//const is used to declare a variable that cannot be reassigned
+const country = "Kenya";
+console.log(country);
+//country = "Uganda"; // This will throw an error because we cannot reassign a const variable
+
+
+//Data types in JavaScript
+//String
+let city = "Nairobi";
+console.log(city);
+
+//Bolean
+let isStudent = true;
+console.log(isStudent);
+
+//null
+let car = null;
+console.log(car);
+
+//undefined
+let address;
+console.log(address);
+
+//Object
+let person = {
+    name: "Mercylina",
+    age: 25,
+    isStudent: true
+};
+console.log(person);
+
+//array
+let fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits);
+console.log(fruits[0]); // Accessing the first element of the array
+console.log(fruits.length); // Getting the length of the array
+
+//symbol
+const x = Symbol();
